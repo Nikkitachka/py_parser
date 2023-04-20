bb3dae1276d2d7f64a4a053ccd14ded1f923dab8
Nik Gur <Nikkitachka@yandex.ru>
2023-04-20 12:01:29 +0300

    Feat(my_parser): Добавлена частичная обработка параметров

--- header_parser/PyVerilog_Parser.py
+++ header_parser/PyVerilog_Parser.py
@@ -20,7 +20,7 @@ from   pyverilog.vparser.ast    import *
 from   pyverilog.vparser.parser import parse
 # /home/nikkitachka/.local/lib/python3.10/site-packages/pyverilog/vparser - расположение pyverilog
 
-class PyVerilog_Parser:
+class Verilog_Parser:
     def __init__(self, filepath: str):
 
         self.filepath = filepath
@@ -173,14 +173,29 @@ class PyVerilog_Parser:
             if f.closed:
                 print('file is closed')
 
+class PyVerilog_Parser:
+    def __init__(self, filepath, filepars: str):
+
+        self.filepath = filepath
+        self.filepars = filepars
+        self.head_signal_typ = []
+        self.head_signal_d_t = []
+        self.head_signal_n_r = []
+        self.head_signal_v_w = []
+        self.head_signal_nam = []
+        self.head_signal_arr = []
+        self.head_signal_clk = []
+        self.head_signal_rst = []
+        self.head_signal_lin = []
+
     #############################################################################################################################
 
     def PyVerilog(self):
         filelist = []
         filelist.append(self.filepath)
 
-        if os.path.isfile("/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt"):
-            os.remove("/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt")
+        if os.path.isfile(self.filepars):
+            os.remove(self.filepars)
             print("Success\n")
         else:
             print("File doesn't exists!")
@@ -190,11 +205,10 @@ class PyVerilog_Parser:
                                 preprocess_define  = [])
 
         ast.show()
+        print("\n")
 
-        # with io.open("/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt", "r") as f:
-
+        # with io.open(self.filepars, "r") as f:
         #     print(f.read())
-
         #     f.close()
         #     if f.closed:
         #         print('file is closed')
@@ -203,14 +217,146 @@ class PyVerilog_Parser:
     
     def ast_parse(self):
 
-        with io.open("/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt", "r") as f:
-            lines = f.readlines()
-            print(len(lines))
-            for i in range(len(lines)):
-                print(lines[i], end = "")
+        print(self.filepath, "\n")
+        print(self.filepars, "\n")
 
-            f.close()
-            if f.closed:
-                print('file is closed')
+        signal_type = ["Input",  "Output"]
+
+        data_type =   ["reg", "wire", "integer", "real", "time", "realtime",                                    # типы данных Verilog
+                       "logic", "bit", "byte", "shortint", "int", "longint", "shortreal"]  
+
+        operation =   []
+
+        with open(self.filepars, "r") as parsed_file:
+            for line in parsed_file:
+                print(line)
+                # Module name parse
+                if ( "ModuleDef" in line ):                                                                     # считывание названия модуля
+                    def_line = re.search('(.*)\n', line).group(1)                                               # захват всей строки в def_line
+                    def_line = re.search("ModuleDef:" + '(.*)', def_line).group(1)                              # ищет в строке 'ModuleDef' + (бесконечно символов)' и удаляет (ModuleDef)
+                    def_line = re.sub(',', '', re.sub('\s', '', re.sub(r'\([^\)]+\)', '', def_line)))           # удаление лишнего (\s - соответствует любому символу пробела ~ [ \t\n\r\f\v]) и запятых
+                    self.module_name = def_line
+                    continue                                                                                    # переход к следующей итерации во внешнем цикле
+
+                # Parameter parse
+                if ( "Parameter" in line ):                                                                     # считывание параметров модуля
+                    self.head_signal_typ.append("Parameter")                                                    # тип сигнала - праметр
+                    self.head_signal_d_t.append('')
+                    self.head_signal_clk.append('')
+                    self.head_signal_rst.append('')
+                    self.head_signal_n_r.append('')                                                             # знаковый/беззнаковый параметр
+                    if ( "True" in line ):
+                        self.head_signal_n_r[len(self.head_signal_n_r)-1] = "signed"
+                    def_line = re.search('(.*)\n', line).group(1)                                               # захват всей строки в def_line
+                    def_line = re.search("Parameter:" + '(.*)', def_line).group(1)                              # ищет в строке 'Parameter' + (бесконечно символов)' и удаляет (Parameter)
+                    self.head_signal_lin.append(re.sub('\s', '',(re.sub(r'^.*?\([a][t][ ]', '', (re.sub(r'\)', '', line))))))  # вычленение номера строки кода с которой работаем 
+                    def_line = re.sub(',', '', re.sub('\s', '', re.sub(r'\,[^\)]+\)', '', def_line)))           # удаление лишнего (\s - соответствует любому символу пробела ~ [ \t\n\r\f\v]) и запятых
+                    self.head_signal_nam.append(def_line)
+                    continue                                                                                    # переход к следующей итерации во внешнем цикле
+
+                if not self.head_signal_lin:
+                    print("List number of code lines is empty")
+                # else:
+                elif ( self.head_signal_lin[-1] == re.sub('\s', '',(re.sub(r'^.*?\([a][t][ ]', '', (re.sub(r'\)', '', line)))))):
+                    print(self.head_signal_lin[-1])
+
+
+
+                # if ( "Rvalue" in line ):
+                #     print("pipi-pupu") 
+
+                    # self.head_signal_typ = [] +
+                    # self.head_signal_d_t = [] +
+                    # self.head_signal_n_r = [] +
+                    # self.head_signal_v_w = [] -
+                    # self.head_signal_nam = [] +
+                    # self.head_signal_arr = [] -
+                    # self.head_signal_clk = [] +
+                    # self.head_signal_rst = [] +
+
+                # searched_strs = ["Input", "Output", "Inout", "Tri", "Wire", "Reg"]
+                # for st in searched_strs:
+                #     if( st in line):
+                #         self.head_signal_typ.append(st)
+                #         signal_str_default = re.search('(.*)\n', line).group(1)                                     # захват всей строки в signal_str_default
+                #         signal_str         = signal_str_default                                                     # дублирование signal_str_default в signal_str
+                #         signal_str         = re.search(st + '(.*)', signal_str_default).group(1)                    # ищет в строке 'input/output' + (бесконечно символов)' и удаляет (input/output)
+                #         signal_str         = re.sub(',', '', re.sub('\s', '', signal_str))                          # удаление пробелов (\s - соответствует любому символу пробела ~ [ \t\n\r\f\v]) и запятых
+
+                #         data_type = ["reg", "wire", "integer", "real", "time", "realtime",                          # типы данных Verilog
+                #                      "logic", "bit", "byte", "shortint", "int", "longint", "shortreal"]             # типы данных SystemVerilog
+                #         self.head_signal_d_t.append('')                                                             # поиск типа данных сигнала ...
+                #         for dt in data_type:
+                #             if (re.search(dt, line)):
+                #                 signal_data_type = dt
+                #                 self.head_signal_d_t[len(self.head_signal_d_t)-1] = signal_data_type
+                #                 signal_str = re.search(dt + '(.*)', signal_str).group(1)                            #                           ...
+
+                #         num_representation = ["signed", "unsigned"]                                                 # представление чисел (знак / беззнак)
+                #         self.head_signal_n_r.append('')                                                             # поиск представления чисел ...
+                #         for nr in num_representation:
+                #             if (re.search(nr, line)):
+                #                 signal_num_representation = nr
+                #                 self.head_signal_n_r[len(self.head_signal_n_r)-1] = signal_num_representation
+                #                 signal_str = re.search(nr + '(.*)', signal_str).group(1)                            #                           ...
+
+                #         signal_vector_width, flag_v_m  = self.get_signal_v_w(signal_str)                            # извлечение ширины (измерения) сигнала
+                #         self.head_signal_v_w.append(signal_vector_width)
+                #         if (flag_v_m):
+                #             signal_str = signal_str.replace(signal_vector_width, '', 1)
+
+                #         signal_array_size, flag_arr  = self.get_signal_arr(signal_str)                              # извлечение размера массива сигнала
+                #         self.head_signal_arr.append(signal_array_size)
+                #         if (flag_arr):
+                #             signal_str = signal_str.replace(signal_array_size, '', 1)
+
+                #         signal_name = signal_str
+                #         self.head_signal_nam.append(signal_name)
+
+                #         signal_clk = ["clk", "CLK", "clock"]                                                        # CLK - используется в параметрах,но пока тут пусть побудет
+                #         self.head_signal_clk.append('')
+                #         for clk in signal_clk:
+                #             if (re.search(clk, signal_name)):
+                #                 self.head_signal_clk[len(self.head_signal_clk)-1] = 'clk'
+                #                 # self.head_signal_clk.append(clk)
+
+                #         signal_rst = ["rst", "RST", "reset"]
+                #         self.head_signal_rst.append('')
+                #         for rst in signal_rst:
+                #             if (re.search(rst, signal_name)):
+                #                 self.head_signal_rst[len(self.head_signal_rst)-1] = 'rst'
+                #                 # self.head_signal_rst.append(rst)
+
+
+
+        # with io.open("/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt", "r") as f:
+        #     lines = f.readlines()
+        #     for i in range(len(lines)):
+        #         # определить первое слово вывода парсера
+        #             # пропускаем Source, Description, ModuleDef, Paramlist, Portlist
+        #         ignore_strs   = ["Source", "Description", "ModuleDef", "Paramlist", "Portlist", "Decl"]
+        #             # находим строки, которые начинаются с Input, Output, Reg..
+        #         searched_strs = ["Input", "Output", "Inout", "Tri", "Wire", "Reg"]
+                
+        #         tab_count = lines[i].count("\t")    # число табов в проверяемой строке
+                
+        #         op   = []                           # операция
+        #         op_A = []                           # первый операнд
+        #         op_B = []                           # второй операнд
+                
+        #         if ( searched_strs in lines[i] ):
+        #             self.head_signal_typ.append(searched_strs)
+
+
+
+        # # построковый вывод из файла
+        # with io.open(self.filepars, "r") as f:
+        #     lines = f.readlines()
+        #     for i in range(len(lines)):
+        #         print(lines[i], end = "")
+
+        #     f.close()
+        #     if f.closed:
+        #         print('\nfile is closed')
 
     #############################################################################################################################
--- header_parser/svparparser_test.py
+++ header_parser/svparparser_test.py
@@ -1,10 +1,12 @@
 # This test uses SVParParser to parse parameters
 # from test modules and print parse log
 
-import PyVerilog_Parser as pyvp
+import PyVerilog_Parser as vp
+# import PyVerilog_Parser as pyvp
 
-parser = pyvp.PyVerilog_Parser("/home/nikkitachka/Desktop/py_parser/header_parser/test_modules/test_module_1.v")
-parser.head_parse()
-parser.parse_file_log()
-parser.PyVerilog()
-parser.ast_parse()
+ver_parser = vp.Verilog_Parser("/home/nikkitachka/Desktop/py_parser/header_parser/test_modules/test_module_4.v")
+pyver_parser = vp.PyVerilog_Parser("/home/nikkitachka/Desktop/py_parser/header_parser/test_modules/test_module_4.v","/home/nikkitachka/Desktop/py_parser/header_parser/out/pyverilog_log.txt")
+ver_parser.head_parse()
+ver_parser.parse_file_log()
+pyver_parser.PyVerilog()  # парсинг файла через pyverilog
+pyver_parser.ast_parse()  # построковое чтение из файла
